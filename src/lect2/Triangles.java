6b48b56b628bb5cf7728b20d7fcc3e48175912cf
KYIV\Sergii_Vasiuta <Autumn2016>
2016-12-14 21:20:02 +0200

    bookExcercise

---
+++ src/lect2/Triangles.java
@@ -0,0 +1,22 @@
+package lect2;
+import java.util.Scanner;
+
+/**
+ * Created by Sergii_Vasiuta on 14.12.2016.
+ */
+public class Triangles {
+
+
+    public static void main(String[] args) {
+
+        int[] dots = new int[10];
+
+        System.out.println((dots.length * (dots.length - 1)*(dots.length - 2) / 6  ));
+
+
+
+
+
+    }
+
+}
